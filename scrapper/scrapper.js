c0f34a6df2f439c3407fb8de9f13bacd855c9bb0
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-04 16:26:11 +0100

    Add scrapper script for extracting enemy data from Wiki
    This script can be used in the browser console on the Brighter Shores Wiki to extract enemy data from specific tables. It parses rows to gather attributes like name, unlock level, combat level, vulnerability, and immunity in a structured JSON format for further use. Adjust the `tableIndex` variable as necessary based on the table location.

---
+++ scrapper/scrapper.js
@@ -0,0 +1,18 @@
+// Use that on the Brighter Shores Wiki via the Console to extract the enemies
+let tableIndex = 1; // Index varies by profession!
+console.log(
+    JSON.stringify(
+        Array.from(document.querySelectorAll("table")[tableIndex].querySelectorAll("tr"))
+            .slice(1).map(row => {
+            const cells = row.querySelectorAll("td");
+            return {
+                name: cells[3]?.textContent?.trim() || "",
+                unlockLevel: parseInt(cells[0]?.textContent?.trim() || ""),
+                combatLevel: parseInt(cells[1]?.textContent?.trim() || ""),
+                vulnerability: `elements.${cells[6]?.textContent?.trim().toLowerCase() || ""}`,
+                immunity: `elements.${cells[6]?.textContent?.trim().toLocaleLowerCase() || ""}`,
+            };
+        }), null, 2)
+        .replaceAll('"elements.none"', 'null')
+        .replace(/"elements\.(\w+)"/gm, `elements.$1`)
+);
