58b2e6efd7de038351bfdcef632154fe574aa487
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-05 00:02:40 +0100

    Add equipment data and accessors to factions
    Updated the Faction model to include equipment categories and items. Implemented utility functions to retrieve equipment and categories by faction name. This enhances the data structure and improves functionality for faction-specific equipment management.

--- src/model/faction.ts
+++ src/model/faction.ts
@@ -1,13 +1,44 @@
+import {
+    cryoknightEquipmentCategories,
+    cryoknightEquipment,
+    Equipment,
+    EquipmentCategory,
+    guardianEquipmentCategories, guardianEquipment, hammermageEquipmentCategories, hammermageEquipment
+} from "@/model/equipment";
+
 export interface Faction {
     name: string;
     craftingProfession: "Blacksmith" | "Bonewright" | "Stonemason";
+    equipmentCategories: EquipmentCategory[];
+    equipment: Equipment[];
 }
 
 export const factions: Faction[] = [
-    {name: "Cryoknight", craftingProfession: "Blacksmith"},
-    {name: "Guardian", craftingProfession: "Bonewright"},
-    {name: "Hammermage", craftingProfession: "Stonemason"},
+    {
+        name: "Cryoknight",
+        craftingProfession: "Blacksmith",
+        equipmentCategories: cryoknightEquipmentCategories,
+        equipment: cryoknightEquipment,
+    },
+    {
+        name: "Guardian",
+        craftingProfession: "Bonewright",
+        equipmentCategories: guardianEquipmentCategories,
+        equipment: guardianEquipment,
+    },
+    {
+        name: "Hammermage",
+        craftingProfession: "Stonemason",
+        equipmentCategories: hammermageEquipmentCategories,
+        equipment: hammermageEquipment,
+    },
 ];
 
 export const craftingProfessionByFactionName: (name: string) => string = (name: string) =>
     factions.find(f => f.name === name)?.craftingProfession || "";
+
+export const equipmentCategoriesByFactionName: (name: string) => EquipmentCategory[] = (name: string) =>
+    factions.find(f => f.name === name)?.equipmentCategories || [];
+
+export const equipmentByFactionName: (name: string) => Equipment[] = (name: string) =>
+    factions.find(f => f.name === name)?.equipment || [];
