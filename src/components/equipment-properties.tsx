bcd8e5606964f32e47a663fc020598ea64faf090
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-07 14:49:38 +0100

    Add tooltips to equipment property icons
    Wrapped element and hand icons in tooltip divs to provide additional context on hover. This improves the UI by giving users more clarity about the meaning of each icon.

--- src/components/equipment-properties.tsx
+++ src/components/equipment-properties.tsx
@@ -7,22 +7,32 @@ const EquipmentProperties = ({category}: { category: string }) => {
 
     return (
         <>
-            <Image
-                src={`/symbols/${c.element.name.toLowerCase()}.png`}
-                alt={c.element.name}
-                className="inline-block mx-1"
-                width={20}
-                height={20}
-            />
-            <Image
-                src={`/symbols/${c.hands}hand.png`}
-                alt={`${c.hands} hand(s)`}
-                className="inline-block"
-                width={20}
-                height={20}
-            />
+            <div className="tooltip tooltip-secondary" data-tip={c.element.name}>
+                <Image
+                    src={`/symbols/${c.element.name.toLowerCase()}.png`}
+                    alt={c.element.name}
+                    className="inline-block mx-1"
+                    width={20}
+                    height={20}
+                />
+            </div>
+            <div className="tooltip tooltip-secondary" data-tip={`${c.hands}-handed`}>
+                <Image
+                    src={`/symbols/${c.hands}hand.png`}
+                    alt={`${c.hands} hand(s)`}
+                    className="inline-block"
+                    width={20}
+                    height={20}
+                />
+            </div>
         </>
     );
 };
 
 export default EquipmentProperties;
+
+/*
+<div className="tooltip" data-tip="hello">
+  <button className="btn">Hover me</button>
+</div>
+ */
