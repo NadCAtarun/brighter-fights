55794dcca3231a7ca301d161bc9ad3c449b80154
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-07 15:46:26 +0100

    JS-0437 Update key usage in map iterations for better uniqueness
    Replaced index-based keys with unique property-based keys in `techStack` and `creditsData` mappings to ensure better React rendering performance and mitigate potential rendering issues. This aligns with best practices for list rendering in React.

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -6,12 +6,12 @@ export default function Page() {
         <div className="p-8 flex flex-col items-center">
             <div className="card w-full max-w-3xl bg-base-100 shadow-xl p-6">
                 <h1 className="text-3xl font-bold text-center mb-6">Credits & Attribution</h1>
-                {creditsData.map((section, index) => (
-                    <div key={index} className="mb-6">
+                {creditsData.map((section) => (
+                    <div key={section.category} className="mb-6">
                         <h2 className="text-xl font-semibold mb-2">{section.category}</h2>
                         <ul className="list-disc pl-5">
-                            {section.items.map((item, idx) => (
-                                <li key={idx} className="flex items-center space-x-3">
+                            {section.items.map((item) => (
+                                <li key={item.name} className="flex items-center space-x-3">
                                     {item.image &&
                                         <Image src={item.image} alt={item.alt} width={50} height={50}
                                                className="rounded-full"/>}
--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
         <div className="p-8 flex flex-col items-center">
             <h2 className="text-3xl font-bold mb-8">🛠️ This website&#39;s technological stack</h2>
             <ul className="steps steps-vertical">
-                {techStack.map((step, index) => (
-                    <TechStackStep key={index} {...step} />
+                {techStack.map((step) => (
+                    <TechStackStep key={step.label} {...step} />
                 ))}
             </ul>
         </div>
