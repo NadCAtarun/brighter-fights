71f1908088e6663bd6744fddef23273505d6bfda
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-08 13:22:30 +0100

    Add tooltips for crafting material quantities
    Introduced tooltips to display the ideal quantity for a full backpack and the quantity needed to craft a single item. This improves clarity and aids users in managing crafting materials more effectively.

--- src/components/crafting-materials.tsx
+++ src/components/crafting-materials.tsx
@@ -1,11 +1,22 @@
 import {Equipment} from "@/model/equipment";
 
 const CraftingMaterials = (equipment: Equipment) => {
+    const materialsCount = equipment.materials
+        .reduce((acc, {quantity}) => acc + quantity, 0);
+    const maxItems = Math.floor(24 / materialsCount);
+
     return (
         <div className="flex flex-col ml-10 text-secondary">
             {equipment.materials.map(({quantity, material}) => (
                 <div key={material}>
-                    {quantity} x {material}
+                    <span className="tooltip mr-2 text-primary"
+                          data-tip="Ideal quantity for full backpack">
+                        [{(maxItems * quantity).toString().padStart(2, '0')}]
+                    </span>
+                    <span className="tooltip mr-2" data-tip="Quantity to craft 1 item">
+                        {quantity}
+                    </span>
+                    x {material}
                 </div>
             ))}
         </div>
