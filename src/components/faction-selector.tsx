d8dadb16aabe71ae978995034fd21ed9772be390
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-10 19:15:23 +0100

    JS-D1001 Add JSDoc comments to FactionSelector component
    Documented the FactionSelector component with JSDoc to provide clarity on its purpose, props, and functionality. This improves code readability and assists developers in understanding the component's usage.

--- src/components/faction-selector.tsx
+++ src/components/faction-selector.tsx
@@ -1,5 +1,15 @@
 import {factions} from "@/model/faction";
 
+/**
+ * FactionSelector is a functional component that renders a dropdown selector
+ * for choosing a faction. It allows users to select a faction from a list of
+ * options and triggers a callback function when a selection is made.
+ *
+ * @param {Object} props - The properties object for the component.
+ * @param {string} props.value - The current selected faction value.
+ * @param {function} props.onSelect - The callback function triggered when a faction is selected.
+ *                                    It receives the selected faction's name as an argument.
+ */
 const FactionSelector = ({value, onSelect}: { value: string, onSelect: (faction: string) => void }) => {
     return (
         <div className="form-control">
