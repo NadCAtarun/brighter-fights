40034db2ac1d3c150004b7e9e63052adeed57604
Nadia Cerezo <nadc.atarun@gmail.com>
2025-02-10 18:12:11 +0100

    Add profession icons to LevelInput component
    Updated LevelInput to display profession-specific icons using Next.js Image component. Added corresponding icon files for all defined professions. Minor formatting adjustments made to ProfessionSelector for consistency.

--- src/components/profession-selector.tsx
+++ src/components/profession-selector.tsx
@@ -1,25 +1,26 @@
 import {Profession, professions} from "@/model/profession";
 
-const ProfessionSelector = ({value, onSelect}: { value: string, onSelect: (profession: string) => void }) => {
-    return (
-        <div className="form-control">
-            <label className="label" htmlFor="profession">
-                <span className="label-text text-xl">Choose your profession:</span>
-            </label>
-            <select
-                id="profession"
-                className="select select-bordered text-xl bg-base-200"
-                value={value}
-                onChange={(e) => onSelect(e.target.value)}
-            >
-                {professions.map((p: Profession) => (
-                    <option key={p.name} value={p.name}>
-                        {p.name}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
-}
+const ProfessionSelector =
+    ({value, onSelect}: { value: string, onSelect: (profession: string) => void }) => {
+        return (
+            <div className="form-control">
+                <label className="label" htmlFor="profession">
+                    <span className="label-text text-xl">Choose your profession:</span>
+                </label>
+                <select
+                    id="profession"
+                    className="select select-bordered text-xl bg-base-200"
+                    value={value}
+                    onChange={(e) => onSelect(e.target.value)}
+                >
+                    {professions.map((p: Profession) => (
+                        <option key={p.name} value={p.name}>
+                            {p.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        );
+    }
 
 export default ProfessionSelector;
