3575e2e4c7c29249a37fb2cb432cf522b68a894e
Nadia Cerezo <nadc.atarun@gmail.com>
2025-03-08 08:54:40 +0100

    Clean slate: remove all files for rewrite

--- src/model/strategy.ts
+++
@@ -1,9 +0,0 @@
-export interface Strategy {
-    name: string;
-    description: string;
-}
-
-export const strategies = [
-    {name: 'vulnerability', description: 'Exploit vulnerability'},
-    {name: 'speed', description: 'Quickest weapon'},
-];
