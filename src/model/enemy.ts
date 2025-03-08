9afaca3c969483f2e23063cc4d9d83dfd9ea0884
Nadia Cerezo <nadc.atarun@gmail.com>
2025-03-08 14:24:06 +0100

    Add info for Hopeport enemies

--- src/model/enemy.ts
+++ src/model/enemy.ts
@@ -1,3 +1,5 @@
+import {Element, elements} from "@/model/element";
+
 export interface Enemy {
     name: string;
     url: string;
@@ -6,3 +8,61 @@ export interface Enemy {
     vulnerability: Element | null;
 }
 
+export const enemies: Enemy[] = [
+    {
+        name: "Sparring Guard",
+        url: "https://brightershoreswiki.org/w/Sparring_Guard",
+        attackStyle: elements.impact,
+        immunity: null,
+        vulnerability: null,
+    },
+    {
+        name: "Deathcrow",
+        url: "https://brightershoreswiki.org/w/Deathcrow",
+        attackStyle: elements.necromae,
+        immunity: elements.necromae,
+        vulnerability: elements.cryonae,
+    },
+    {
+        name: "Thief",
+        url: "https://brightershoreswiki.org/w/Thief",
+        attackStyle: elements.cryonae,
+        immunity: elements.cryonae,
+        vulnerability: elements.arborae,
+    },
+    {
+        name: "Goblin Soldier",
+        url: "https://brightershoreswiki.org/w/Goblin_Soldier",
+        attackStyle: elements.infernae,
+        immunity: elements.infernae,
+        vulnerability: elements.tempestae,
+    },
+    {
+        name: "Goblin Chief",
+        url: "https://brightershoreswiki.org/w/Goblin_Chief",
+        attackStyle: elements.infernae,
+        immunity: elements.infernae,
+        vulnerability: elements.tempestae,
+    },
+    {
+        name: "Hairbeast",
+        url: "https://brightershoreswiki.org/w/Hairbeast",
+        attackStyle: elements.tempestae,
+        immunity: elements.tempestae,
+        vulnerability: elements.necromae,
+    },
+    {
+        name: "Carnivorous Plant",
+        url: "https://brightershoreswiki.org/w/Carnivorous_Plant",
+        attackStyle: elements.arborae,
+        immunity: elements.arborae,
+        vulnerability: elements.cryonae,
+    },
+    {
+        name: "Ghoul",
+        url: "https://brightershoreswiki.org/w/Ghoul",
+        attackStyle: elements.necromae,
+        immunity: elements.necromae,
+        vulnerability: elements.arborae,
+    },
+];
