5620891e4feb3fb49cfe7f96a4a3ef493c79cc37
Nadia Cerezo <nadc.atarun@gmail.com>
2025-03-12 16:33:07 +0100

    Add rarity selector to Home page
    Introduce a new `RaritySelector` component to allow users to toggle between "Rare" and "Epic" crafting options. Persist the selected rarity in localStorage and initialize it on page load for a consistent user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import FactionSelector from "@/components/faction-selector";
 import {useCallback, useEffect, useState} from "react";
 import EnemySelector from "@/components/enemy-selector";
 import {Enemy, enemyByName} from "@/model/enemy";
-
+import RaritySelector from "@/components/rarity-selector";
 
 export default function Home() {
     const [faction, setFaction] = useState('');
     const [enemy, setEnemy] = useState<Enemy | null>(null);
+    const [rarity, setRarity] = useState<'Rare' | 'Epic'>('Epic');
 
     useEffect(() => {
         const faction = localStorage.getItem('faction') || '';
@@ -16,6 +17,9 @@ export default function Home() {
 
         const enemy = localStorage.getItem('enemy') || '';
         setEnemy(enemyByName(enemy));
+
+        const rarity = localStorage.getItem('rarity') || 'Epic';
+        setRarity(rarity === 'Rare' ? 'Rare' : 'Epic');
     }, [faction, enemy]);
 
     const handleFactionChange = useCallback(
@@ -30,10 +34,17 @@ export default function Home() {
             setEnemy(enemy);
         }, []);
 
+    const handleRarityChange = useCallback(
+        (rarity: 'Rare' | 'Epic') => {
+            localStorage.setItem('rarity', rarity);
+            setRarity(rarity);
+        }, []);
+
     return (
         <>
             <FactionSelector value={faction} onSelect={handleFactionChange}/>
             <EnemySelector value={enemy} onSelect={handleEnemyChange}/>
+            <RaritySelector value={rarity} onSelect={handleRarityChange}/>
         </>
     );
 }
